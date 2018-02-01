a9f7d17c86bc1182bfc45806c0ae09674788af30
nmorales9 <nmorales9@mail.csuchico.edu>
2018-02-01 11:55:49 -0800

    describe what is new in your own words

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,24 @@
-//Authors: 
+//Authors: Nysa Morales 
 #include <iostream>
 #include <string>
-
+using namespace std;
 int main()
 {
+    string car;
+    int angle;
+    double seconds;
+    char mark;
+        cout<<"give me a name of a car \n";
+        cin>>car;
+        cout<<"give me a name of a angle \n";
+        cin>>angle;
+        cout<<"give me a decimal number \n";
+        cin>>seconds;
+        cout<<"give me a punctuation mark \n";
+        cin>>mark;
+cout<<"Jump in the "<<car<<" hit a sharp turn at an angle of "<<angle<<".\n";
+cout<<"You might have passed me on the turn, but I beat you by "<<seconds<<"seconds.\n";
+cout<<"sucka "<<mark<<endl;
 
   return 0;
 }
